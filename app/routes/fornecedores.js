12ab6a425cc5cf0c3a99d221ac388e855f7db70f
juba021 <junior.ferrarese@gmail.com>
2018-11-25 17:03:49 -0200

    fornecedor edit

--- app/routes/fornecedores.js
+++ app/routes/fornecedores.js
@@ -2,6 +2,8 @@ var Fornecedor = require('../models/fornecedor.js');
 var access = require('../access.js');
 
 module.exports = function(app) {
+
+    //// Lista fornecedores
     app.get('/fornecedores', function(req, res) {
         Fornecedor.find({}, function (err, fornecedores) {
             console.log(fornecedores);
@@ -13,13 +15,29 @@ module.exports = function(app) {
             }
         })
     });    
-
+    
+    //// página de cadastro de fornecedores.
     app.get('/fornecedores/new', function(req, res) {
         res.render("fornecedores/addFornecedor");
     }); 
 
+    //// Página editar fornecedor.
+    app.get('/fornecedores/edit/:_id', function(req, res) {
+        Fornecedor.findById({_id: req.params._id}, function(err, fornecedor) {
+            console.log(fornecedor);
+            
+            if (err) {
+                access.redirect(req, res, true, 'error', null);
+            } else {               
+                res.render("fornecedores/editFornecedor", { fornecedor: fornecedor });
+            }
+        })
+    });  
+
+    //// Função de cadastrar fornecedor.
     app.post('/fornecedores/create', function(req, res) {
-        console.log("chegou");
+       
+        
         if (req.body.CNPJ !== undefined && req.body.CNPJ !== "" &&
             req.body.nome !== undefined && req.body.nome !== "" &&
             req.body.nomeFantasia !== undefined && req.body.nomeFantasia !== "" &&
@@ -27,8 +45,9 @@ module.exports = function(app) {
             req.body.uf !== undefined && req.body.uf !== "" &&
             req.body.telefone !== undefined && req.body.telefone !== "" &&
             req.body.email !== undefined && req.body.email !== "" &&
-            req.body.description !== undefined && req.body.description !== "") {
-
+            req.body.descricao !== undefined && req.body.descricao !== "") {
+                
+                
             var fornecedor = new Fornecedor();
             fornecedor.nome = req.body.nome
             fornecedor.nomeFantasia = req.body.nomeFantasia
@@ -47,33 +66,48 @@ module.exports = function(app) {
                 }
             })
         }   
-    }); 
-
-    app.get('/fornecedores/view', function(req, res) {
-        console.log(req.body.nome);
-        Fornecedor.findById({_id: "5bf9c043fe63ca21c09ba3eb"}, function(err,obj ) {
-            res.json(obj)
-        })
-
-
-
-    }); 
-
-    
-
-    app.get('/fornecedores/edit/:_id', function(req, res) {
-        Fornecedor.findById({_id: req.params._id}, function(err, fornecedor) {
-            if (err) {
-                access.redirect(req, res, true, 'error', null);
-            } else {
-
-                res.render("fornecedores/editFornecedor", { fornecedor: fornecedor });
-            }
-        })
-    });   
-
-    ///delete isso
-    app.get("/teste", function(req, res) {
-        res.render("fornecedores/teste")
     });
+
+
+    app.post('/fornecedores/update/:_id', function(req, res) {
+        if (req.body.CNPJ !== undefined && req.body.CNPJ !== "" &&
+            req.body.nome !== undefined && req.body.nome !== "" &&
+            req.body.nomeFantasia !== undefined && req.body.nomeFantasia !== "" &&
+            req.body.cidade !== undefined && req.body.cidade !== "" &&
+            req.body.uf !== undefined && req.body.uf !== "" &&
+            req.body.telefone !== undefined && req.body.telefone !== "" &&
+            req.body.email !== undefined && req.body.email !== "" &&
+            req.body.descricao !== undefined && req.body.descricao !== "") {
+                console.log(req.params._id);
+                
+            Fornecedor.findById({_id: req.params._id}, function(err, fornecedor) {
+                console.log(fornecedor);
+                
+                if (err) {
+                    access.redirect(req, res, true, 'error', null);
+                } else {
+                    fornecedor.nome = req.body.nome
+                    fornecedor.nomeFantasia = req.body.nomeFantasia
+                    fornecedor.CNPJ = req.body.CNPJ
+                    fornecedor.endereco.cidade = req.body.cidade;
+                    fornecedor.endereco.uf = req.body.uf
+                    fornecedor.telefone = req.body.telefone
+                    fornecedor.email = req.body.email
+                    fornecedor.descricao = req.body.descricao
+                    console.log(fornecedor);
+                
+                    fornecedor.save( function(err, obj) {
+                        if (err) {
+                            access.redirect(req, res, true, 'error', null);
+                        } else {
+                            res.sendStatus(200);
+                        }
+                    })
+                }
+            })
+        }
+    });  
+
+ 
+
 }
