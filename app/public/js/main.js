31732570a3f541bc8bbd9a2c8673fc4a5f63df03
Luis Carlos Ferrarese Junior <luis.ferrarese@pampa.compasso>
2018-11-29 15:08:14 -0200

    snackbar working

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,10 +1,14 @@
-function snackbar (message, time) {
+function snackbar (message, time, code) {
 
+	if (code === 'success') {
+		var notification = document.getElementById('success-snackbar');
+	} else if (code === 'error') {
+		var notification = document.getElementById('error-snackbar');
+	}
 
-	var notification = document.querySelector('.mdl-js-snackbar');
 	var data = {
 		message: message,
-		timeout: time * 1000
+		timeout: time * 1000,
 	};
 	notification.MaterialSnackbar.showSnackbar(data);
 }
--- app/routes/vendas.js
+++ app/routes/vendas.js
@@ -25,9 +25,16 @@ module.exports = function(app) {
     });  
 
     app.get('/venda/edit/:_id', function(req, res) {
-        
-        Venda.findById({_id: req.params._id}).populate('produtos').exec(function(err, venda) {
-            console.log(venda.produtos);
+        ////deep populate 
+        Venda.findById({_id: req.params._id}).populate({
+                path: 'produtos',
+                populate: {path: 'fornecedor'}
+            }).populate({
+                path: 'produtos',
+                populate: {path: 'categoria'}
+            })
+        .exec(function(err, venda) {
+            console.log(venda);
             
             if (err) {
                 access.redirect(req, res, true, 'error', null);
@@ -82,9 +89,7 @@ module.exports = function(app) {
                 venda.total = req.body.total;
 
                 for (const produto of req.body.produtosVenda) {
-
                     venda.produtos.push(produto._id)
-                    
                 }
 
                 venda.save(function(err, obj) {
@@ -97,6 +102,33 @@ module.exports = function(app) {
                 });
     
         }
+    });
+
+
+
+    app.put('/venda/update/:_id', function(req, res) {
+        if (req.body.cliente !== undefined && req.body.cliente !== "" &&
+            req.body.total !== undefined && req.body.total > 0 &&
+            req.body.produtosVenda.length > 0) {
+                
+            Venda.findById({_id: req.params._id}, function(err, venda) {
+                venda.cliente = req.body.cliente;
+                venda.total = req.body.total;
+                venda.produtos = [];
+                for (const produto of req.body.produtosVenda) {
+                    venda.produtos.push(produto._id)
+                }
+
+                venda.save( function(err, obj) {
+                    if (err) {
+                        access.redirect(req, res, true, 'error', null);
+                    } else {
+                        res.json(obj)
+                    }
+                });
+
+            });
+        }   
     });
 
 }
